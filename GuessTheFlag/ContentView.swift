bc3d8f5e29775c679e77025cbd6f6d6e59565fb7
Andrei Korikov <kpanic666@gmail.com>
2021-09-17 17:37:22 +0500

    Add text for VoiceOver

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -13,6 +13,22 @@ struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var userScore = 0
+    @State private var animationAmount = 0.0
+    @State private var animationOpacity = 1.0
+  
+  let labels = [
+    "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+    "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+    "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+    "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+    "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+    "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+    "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+    "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+    "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+    "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+    "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+  ]
     
     var body: some View {
         ZStack {
@@ -37,7 +53,14 @@ struct ContentView: View {
                             .renderingMode(.original)
                             .clipShape(Capsule())
                             .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .rotation3DEffect(
+                                .degrees(animationAmount),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
                             .shadow(color: .black, radius: 10)
+                            .opacity(animationOpacity)
+                            .animation(number == correctAnswer ? .default : nil)
+                          .accessibility(label: Text(labels[countries[number], default: "Unknown flag"]))
                     }
                 }
                 
@@ -52,19 +75,21 @@ struct ContentView: View {
                     askQuestion()
                 })
             }
-            
         }
     }
     
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
+            animationAmount += 360
+            animationOpacity = 1.0
             scoreTitle = "Correct"
             userScore += 1
         } else {
+            animationOpacity = 0.25
             scoreTitle = "Wrong! That’s the flag of \(countries[number])"
             userScore -= 1
         }
-
+        
         showingScore = true
     }
     
